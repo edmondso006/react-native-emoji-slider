9e40f5ffb81e1df26b914c7068e493a705c5c29f
Jeff Edmondson <edmondso006@knights.gannon.edu>
2021-09-02 17:50:02 -0400

    Emoji Slider

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Slider from '@react-native-community/slider';
 
 export default function App() {
+
+  const [rating, setRating] = React.useState(3);
+
+  const getRatingEmoji = () => {
+    if (rating === 1){ return '😡' }
+
+    if (rating === 2) { return '😫' } 
+
+    if (rating === 3) { return '😶' } 
+
+    if (rating === 4)  { return '🙂' } 
+
+    if (rating === 5) { return '😁' } 
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Slider
+        style={{width: 200, height: 40}}
+        minimumValue={1}
+        maximumValue={5}
+        step={1}
+        onValueChange={setRating}
+      />  
+      <Text style={{ fontSize: 50 }}>
+        {getRatingEmoji()}
+      </Text>
     </View>
   );
 }
